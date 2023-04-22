6a7fc234d285c7642081fe4fb4f7e49401c374b9
Vsevolod Romashov <avs.rom@gmail.com>
2023-04-22 11:36:41 +0300

    Init BRT module

---
+++ BRT/src/main/java/com/romashov/brt/BrtApplication.java
@@ -0,0 +1,13 @@
+package com.romashov.brt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrtApplication.class, args);
+    }
+
+}
